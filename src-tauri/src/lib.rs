f02ab74e0db89d8b0975fc81750af256b2e34f28
Iñaki Gisbert <igisbertmartinez@gmail.com>
2026-06-29 19:52:56 +0200

    feat: agregar soporte para el estado de la ventana en la aplicación Tauri

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,6 +6,7 @@ pub fn run() {
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .invoke_handler(tauri::generate_handler![
             commands::fs::pick_folder,
             commands::fs::scan_models,
